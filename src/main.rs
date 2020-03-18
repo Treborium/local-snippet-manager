79cf4a9c015edf73d19e0a1803fa6698e55ee840
Robert Fuchs <robertfuchs97@gmail.com>
2020-03-18 13:18:50 +0100

    Create function to execute command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use structopt::StructOpt;
-use serde_json::{Value};
-use serde::{Serialize, Deserialize};
 
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
-use std::collections::HashMap;
+use std::io::{self, Write, BufReader};
+use std::process::{Command, Output};
 
 type LSM = HashMap<String, HashMap<String, String>>;
+
+fn run_cmd(cmd: &str) -> Result<(), Box<dyn Error>> {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(cmd)
+        .output()?;
+
+    std::io::stdout().write_all(&output.stdout)?;
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Open the file in read-only mode with buffer.
@@ -15,8 +26,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let reader = BufReader::new(file);
 
     // Read the JSON contents of the file as an instance of `User`.
-    let json = serde_json::from_reader::<BufReader<File>, LSM> (reader)?;
+    let json = serde_json::from_reader::<BufReader<File>, LSM>(reader)?;
     println!("{:#?}", json);
-
-    Ok(())
+    run_cmd(&json["Navigation"]["List Content"])
 }
