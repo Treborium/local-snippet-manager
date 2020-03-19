44a930021e09ce749f37ea81e2bc26eefe63a0c3
Robert Fuchs <robertfuchs97@gmail.com>
2020-03-19 16:27:05 +0100

    Add flag to keep emulator alive after command finished

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::{BufReader, Write};
 use std::process::Command;
 
-use clap::{load_yaml, App};
+use clap::{load_yaml, App, ArgMatches};
 
 mod display;
 
@@ -12,35 +12,43 @@ type LSM = HashMap<String, HashMap<String, String>>;
 
 const DEFAULT_TERMINAL: &str = "/usr/bin/kitty";
 
-fn run(cmd: &str) -> Result<std::process::Child, std::io::Error> {
-    Command::new(DEFAULT_TERMINAL)
-        // .arg("--hold")  // Keep the terminal emulator alive after command finished
-        .arg(cmd)
-        .spawn()
+fn run(cmd: &str, hold: bool) -> Result<std::process::Child, std::io::Error> {
+    let mut args: Vec<String> = Vec::new();
+
+    if hold {
+        args.push(String::from("--hold"));
+    }
+
+    Command::new(DEFAULT_TERMINAL).args(&args).arg(cmd).spawn()
 }
 
-fn sel(commands: &HashMap<String, String>) -> Result<(), Box<dyn Error>> {
+fn sel(commands: &HashMap<String, String>, matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let mut fzf = Command::new("fzf")
-    // .args(&["--preview=", "hello world"])
-    // .arg("--tac")
-    .stdin(std::process::Stdio::piped())
-    .stdout(std::process::Stdio::piped())
-    .spawn()?;
+        // .args(&["--preview=", "hello world"])
+        // .arg("--tac")
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()?;
 
-    let options: Vec<String> = commands.into_iter()
+    let options: Vec<String> = commands
+        .iter()
         // .map(|(key, value)| display::format_line(key, value, 20, 60))
         .map(|(key, _)| key.clone())
         .collect();
 
     let stdin = fzf.stdin.as_mut().unwrap();
-    stdin.write_all(options.join("\n").as_bytes())
+    stdin
+        .write_all(options.join("\n").as_bytes())
         .expect("Error while listing available commands.");
 
     let out = fzf.wait_with_output().unwrap();
     let choice = String::from_utf8(out.stdout).unwrap();
 
     if !choice.is_empty() {
-        let _ = run(commands.get(&choice[..choice.len() - 1]).unwrap());
+        let _ = run(
+            commands.get(&choice[..choice.len() - 1]).unwrap(),
+            matches.is_present("hold"),
+        );
     }
 
     Ok(())
@@ -65,11 +73,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::from(yml).get_matches();
     match matches.subcommand() {
         ("run", Some(run_matches)) => {
-            let _ = run(run_matches.value_of("command").unwrap());
+            let _ = run(run_matches.value_of("command").unwrap(), false);
         }
         ("ls", Some(_ls_matches)) => println!("{:#?}", commands),
-        ("sel", Some(_sel_matches)) => { 
-            let _ = sel(&commands);
+        ("sel", Some(sel_matches)) => {
+            let _ = sel(&commands, &sel_matches);
         }
         ("", None) => println!("No subcommand was used"), // If no subcommand was usd it'll match the tuple ("", None)
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
