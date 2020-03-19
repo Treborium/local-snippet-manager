56d08011757474a12213cd22736e8dd43ccd7612
Robert Fuchs <robertfuchs97@gmail.com>
2020-03-19 17:34:55 +0100

    Allow to run commands with more than one parameter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, Write};
 use std::process::Command;
@@ -11,31 +10,35 @@ type LSM = HashMap<String, HashMap<String, String>>;
 const DEFAULT_TERMINAL: &str = "/usr/bin/kitty";
 
 fn execute(cmd: &str, hold: bool) -> Result<std::process::Child, std::io::Error> {
-    let mut args: Vec<String> = Vec::new();
+    let mut shell_args: Vec<String> = Vec::new();
+    let sub_commands: Vec<&str> = cmd.split(' ').collect();
 
     if hold {
-        args.push(String::from("--hold"));
+        shell_args.push(String::from("--hold"));
     }
 
-    Command::new(DEFAULT_TERMINAL).args(&args).arg(cmd).spawn()
+    Command::new(DEFAULT_TERMINAL)
+        .args(&shell_args)
+        .arg(sub_commands[0])
+        .args(&sub_commands[1..])
+        .spawn()
 }
 
-fn run(commands: &HashMap<String, String>, matches: &ArgMatches) -> Result<(), std::io::Error> {
+fn run(commands: &HashMap<String, String>, matches: &ArgMatches) {
     let input = matches.value_of("command").unwrap();
 
     match commands.get(input) {
         Some(cmd) => {
             let _ = execute(cmd, matches.is_present("hold"));
-            Ok(())
         }
-        None => return Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            format!("<{}> not found! Please make sure there are no typos.", input)
-        )),
+        None => {
+            eprintln!("'{}' not found! Please make sure there are no typos.", input);
+            std::process::exit(-1)
+        }
     }
 }
 
-fn sel(commands: &HashMap<String, String>, matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
+fn sel(commands: &HashMap<String, String>, matches: &ArgMatches) {
    let choice = make_choice(&commands);
 
     if !choice.is_empty() {
@@ -44,8 +47,6 @@ fn sel(commands: &HashMap<String, String>, matches: &ArgMatches) -> Result<(), B
             matches.is_present("hold"),
         );
     }
-
-    Ok(())
 }
 
 fn print(commands: &HashMap<String, String>) {
@@ -79,13 +80,15 @@ fn make_choice(commands: &HashMap<String, String>) -> String {
     String::from_utf8(out.stdout).unwrap()
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     // Open the file in read-only mode with buffer.
-    let file = File::open("lsm.json")?;
+    let file = File::open("lsm.json")
+        .expect("Could not open lsm file.");
     let reader = BufReader::new(file);
 
     // Read the JSON contents of the file as an instance of `LSM`.
-    let json: LSM = serde_json::from_reader::<BufReader<File>, LSM>(reader)?;
+    let json: LSM = serde_json::from_reader::<BufReader<File>, LSM>(reader)
+        .expect("Could not parse lsm file.");
     let mut commands: HashMap<String, String> = HashMap::new();
 
     for (_, sub_map) in json {
@@ -98,13 +101,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::from(yml).get_matches();
 
     match matches.subcommand() {
-        ("run", Some(run_matches)) => run(&commands, &run_matches)?,
+        ("run", Some(run_matches)) => run(&commands, &run_matches),
         ("ls", Some(_ls_matches)) => println!("{:#?}", commands),
-        ("sel", Some(sel_matches)) => sel(&commands, &sel_matches)?,
+        ("sel", Some(sel_matches)) => sel(&commands, &sel_matches),
         ("print", Some(_print_matches)) => print(&commands),
         ("", None) => println!("No subcommand was used"), // If no subcommand was usd it'll match the tuple ("", None)
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
     }
-
-    Ok(())
 }
